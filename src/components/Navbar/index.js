62394c2f0b4f739fc34f51022d31dee9d904067c
olawale-o <omoogunolawale@gmail.com>
2022-03-11 06:15:08 +0100

    User Link

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.png';
 import { AccessKey } from '../Svg';
 import './style.css';
@@ -7,29 +8,29 @@ const Navbar = () => {
     <header className="header">
       <nav className="nav">
         <div className="nav__logo">
-          <a href="/" className="nav__logo-link">
+          <Link to="/" className="nav__logo-link">
             <img src={logo} alt="logo" className="nav__logo-img" />
-          </a>
+          </Link>
         </div>
         <ul className="links">
           <li className="links__item">
-            <a href="/" className="links__link">Home</a>
+            <Link to="/" className="links__link">Home</Link>
           </li>
           <li className="links__item">
-            <a href="/" className="links__link">Services</a>
+            <Link to="/#" className="links__link">Services</Link>
           </li>
           <li className="links__item">
-            <a href="/" className="links__link">About us</a>
+            <Link to="/#" className="links__link">About us</Link>
           </li>
         </ul>
         <ul className="buttons">
           <li className="button__item">
-            <a href="/#" className="btn__link btn__primary">
+            <Link to="login" className="btn__link btn__primary">
               <span>
                 <AccessKey fillColor="#fff" />
               </span>
               <span>Login</span>
-            </a>
+            </Link>
           </li>
           <li className="button__item">
             <a href="/#" className="btn__link btn__outline">
