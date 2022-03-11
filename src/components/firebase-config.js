63d026cb634767e18cda21b99e144d8d0988271d
olawale-o <omoogunolawale@gmail.com>
2022-03-11 08:24:48 +0100

    Config firebase

---
+++ src/components/firebase-config.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_GOOGLE_KEY,
+  authDomain: "riverpod-crime.firebaseapp.com",
+  projectId: "riverpod-crime",
+  storageBucket: "riverpod-crime.appspot.com",
+  messagingSenderId: "1029610132870",
+  appId: "1:1029610132870:web:c105ca16ed6f0754554807",
+  measurementId: "G-3MM2Z2TC8K"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
