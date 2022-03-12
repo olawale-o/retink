a9f6ab06da1772ec95b1290f5ff7dcca90ebc4f6
olawale-o <omoogunolawale@gmail.com>
2022-03-12 12:21:14 +0100

    Configure firestore

--- src/components/firebase-config.js
+++ src/components/firebase-config.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore'
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_GOOGLE_KEY,
@@ -12,5 +13,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const store = getFirestore(app);
 
 export const auth = getAuth(app);
