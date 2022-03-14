8753448f80d7a1b2b27b61f1f5dccb42d7dfdc57
olawale-o <omoogunolawale@gmail.com>
2022-03-14 11:24:16 +0100

    Add minimal styling

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,42 +1,41 @@
 import './style.css';
 import character from '../../assets/character.png';
+import { Hat } from '../../components/Svg';
 const Home = () => (
   <div className="home">
-    <div className="container">
+    <div className="hero__container">
       <div className="hero">
         <div className="hero__left">
           <h6 className="hero__title">What are you looking for?</h6>
           <div className="card">
-            <div></div>
-            <div className="right__card">
-              <h2 className="card__title">Help me create a Marketing Plan!</h2>
-              <p className="card__text">The Arctic ocean freezes every winter and much of the see ice then thaws every summer</p>
+            <div className="svg">
+              <Hat />
             </div>
+            <h2 className="card__title">Help me create a Marketing Plan!</h2>
+            <p className="card__text">The Arctic ocean freezes every winter and much of the see ice then thaws every summer</p>
           </div>
           <div className="card">
-            <div></div>
-            <div className="right__card">
-              <h2 className="card__title">I know what i am looking for.</h2>
-              <p className="card__text">The Arctic ocean freezes every winter and much of the see ice then thaws every summer</p>
+            <div className="svg">
+              <Hat />
             </div>
+            <h2 className="card__title">I know what i am looking for.</h2>
+            <p className="card__text">The Arctic ocean freezes every winter and much of the see ice then thaws every summer</p>
           </div>
         </div>
         <div className="hero__right">
-          <div className="card">
+          <div className="lg__card">
             <p className="card__text">
               Hi there! Need help in creating a Marketing plan for your business? I can help you to create one using Retink AI engine.
             </p>
           </div>
-          <div className="card">
+          <div className="sm__card">
             <p className="card__text">
               Click on the options to get started
             </p>
           </div>
         </div>
       </div>
-      <div className="character">
-        <img src={character} alt="character" />
-      </div>
+      <img src={character} alt="character" className="hero__img" />
     </div>
 
     <div className="services">
