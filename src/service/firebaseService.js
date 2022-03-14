648218d4a0dbccc4345ed1fb7b246fea1d5c8341
olawale-o <omoogunolawale@gmail.com>
2022-03-14 14:52:37 +0100

    Create firebase service

---
+++ src/service/firebaseService.js
@@ -0,0 +1,34 @@
+import {
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from 'firebase/auth';
+
+import { auth, googleProvider } from '../firebase-config';
+
+export const signInWithGoogle = async () => {
+  try {
+    const { user } = await signInWithPopup(auth, googleProvider);
+    return user;
+  } catch(e) {
+    throw new Error(e);
+  }
+};
+
+export const logInWithEmailAndPassword = async (email, password) => {
+  try {
+    const { user } = await signInWithEmailAndPassword(auth, email, password);
+    return user;
+  } catch(e) {
+    throw new Error(e);
+  }
+};
+
+export const createUserAccount = async (email, password) => {
+  try {
+    const { user } = await createUserWithEmailAndPassword(auth, email, password);
+    return user;
+  } catch(e) {
+    throw new Error(e);
+  }
+};
