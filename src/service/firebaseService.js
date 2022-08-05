0114292dfe46883f55ddd5756c84662b68cef7be
olawale-o <omoogunolawale@gmail.com>
2022-08-05 15:59:34 +0100

    Modify createUserAccount in firebase service

--- src/service/firebaseService.js
+++ src/service/firebaseService.js
@@ -3,8 +3,8 @@ import {
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
 } from 'firebase/auth';
-
 import { firebaseAuth, googleProvider } from '../firebase-config';
+import { post } from '../api/axios';
 
 export const signInWithGoogle = async () => {
   try {
@@ -27,8 +27,20 @@ export const logInWithEmailAndPassword = async (email, password) => {
 export const createUserAccount = async (email, password) => {
   try {
     const { user } = await createUserWithEmailAndPassword(firebaseAuth, email, password);
+    if (user) {
+      const dbUser = await post(
+        'user/signup', {
+          email: user.email,
+          full_name: user?.displayName || 'full name',
+          uid: user.uid,
+          provider_id: user.providerId,
+          photo_url: user?.photoURL,
+        });
+      return dbUser;
+    }
     return user;
   } catch(e) {
+    console.log(e);
     throw new Error(e.code);
   }
 };
