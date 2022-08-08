b5b99fa105a7460ef62fb429d67efe0e6989d6a8
olawale-o <omoogunolawale@gmail.com>
2022-08-08 08:35:50 +0100

    Remove firebase config

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore'
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_GOOGLE_KEY,
-  authDomain: "riverpod-crime.firebaseapp.com",
-  projectId: "riverpod-crime",
-  storageBucket: "riverpod-crime.appspot.com",
-  messagingSenderId: "1029610132870",
-  appId: "1:1029610132870:web:c105ca16ed6f0754554807",
-  measurementId: "G-3MM2Z2TC8K"
+  authDomain: process.env.REACT_APP_RETINK_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_RETINK_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_RETINK_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_RETINK_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_RETINK_APP_ID,
+  measurementId: process.env.REACT_APP_RETINK_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
