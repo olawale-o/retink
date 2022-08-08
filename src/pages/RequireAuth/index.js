c4f597380025f7bbd133abfaeb9fa01eb0c963b7
olawale-o <omoogunolawale@gmail.com>
2022-08-08 07:45:32 +0100

    Modify RequireAuth page

--- src/pages/RequireAuth/index.js
+++ src/pages/RequireAuth/index.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
-import { AuthContext } from "../../context";
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuth } from '../../hooks';
 
 const RequireAuth = () => {
-  const { user } = React.useContext(AuthContext);
+  const { user } = useAuth();
   return (
     <>
       {user?.accessToken ? <Outlet /> : <Navigate to="/auth" replace />}
